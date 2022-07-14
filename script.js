7d5e1f51dd85cca6d430109bff7bb93178a08221
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-07-13 21:20:59 -0300

    Requisito 1, 2 e 3

---
+++ script.js
@@ -0,0 +1,14 @@
+const btnSend = document.getElementById('btn-send');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+function validation(event) {
+  event.preventDefault();
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+btnSend.addEventListener('click', validation);
